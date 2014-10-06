b2b6416469cf322400d981a5c208cd4556bc096c
tegon <leonardotegon@clickjogos.com.br>
2014-10-06 17:20:09 -0300

    [project] add project

---
+++ app/src/main/java/br/com/clickjogos/ssoexample/Constants.java
@@ -0,0 +1,8 @@
+package br.com.clickjogos.ssoexample;
+
+public class Constants {
+    public static String OAUTH_URL = "http://accounts.gruponzn.com/sso";
+    public static String KEY = "df6ebd99820da3ee1fe81d84556cc802";
+    public static String SECRET = "7cd2a26f2f72a560faf201de1618b9b7";
+    public static String AUTHORIZE_URL = OAUTH_URL + "/" + KEY;
+}
